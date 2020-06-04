7a7b208756fb788c9b6d2ab93ef0541d69c3f7a6
blackwhite2018 <39052924+blackwhite2018@users.noreply.github.com>
2020-06-04 15:38:56 +0300

    Updates

---
+++ precache-manifest.08fd864849f386e49151184dbe95a961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd736b53dee0e606e14b91e8cccd40b6",
+    "url": "/Gallery/index.html"
+  },
+  {
+    "revision": "ee2422d016fca2cfe39b",
+    "url": "/Gallery/static/css/main.da9c25e3.chunk.css"
+  },
+  {
+    "revision": "ba77c5e65759b9e4678f",
+    "url": "/Gallery/static/js/2.0616425c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gallery/static/js/2.0616425c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2422d016fca2cfe39b",
+    "url": "/Gallery/static/js/main.8da59982.chunk.js"
+  },
+  {
+    "revision": "f21d38f27f9216046180",
+    "url": "/Gallery/static/js/runtime-main.a0976d91.js"
+  }
+]);
